701bbab2c9e145c0f0ed17986fc5bed55b451bdd
Leydi <ljpatinos@unal.edu.co>
2025-01-25 11:52:13 -0500

    Update aplication.py

--- aplication.py
+++ aplication.py
@@ -4,5 +4,5 @@ def main():
   st.title ("Clasificación de la base de datos mnist")
   st.markdown("Sube una imagen para clasificar"9
 
-  if __name__='__main__':
+  if __name__=='__main__':
     main()
