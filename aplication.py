b9dd84e4242514cb5d316c26911e83ecf3f5246b
Leydi <ljpatinos@unal.edu.co>
2025-01-28 18:33:22 -0500

    Update aplication.py

--- aplication.py
+++ aplication.py
@@ -1,10 +1,17 @@
 import streamlit as st
 from PIL import Image
+from tensorflow.keras.preprocessing.image import img_to_array
+
+def preprocess_image(image):
+  image ) image.convert('L') #convertir a escala de grises
+  image = image.resize((28,28))
+  image_array = img_to_array(image) / 255.0
+  image_array = np.expand_dims(image_array, axis=0)
+  return image_array
 
 def main():
   st.title("Clasificación de la base de datos MNIST")
   st.markdown("Sube una imagen para clasificar")
-  2
   uploaded_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG:)", type = ["jpg", "png", "jpeg"])
 
 
@@ -12,6 +19,8 @@ def main():
     image = Image.open(uploaded_file)
     st.image(image, caption = "imagen subida")
 
+    preprocessed_image = preprocess_image(image)
+    st.image(preprocessed_image, caption = "imagen subida")
 
 if __name__ == "__main__":
   main()
