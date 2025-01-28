9df4196ac1cf1334d2d2eb8827ad1959a06eb39c
Leydi <ljpatinos@unal.edu.co>
2025-01-28 18:12:27 -0500

    Update aplication.py

--- aplication.py
+++ aplication.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 def main():
-  st.title ("Clasificación de la base de datos mnist")
+  st.title("Clasificación de la base de datos MNIST")
   st.markdown("Sube una imagen para clasificar")
 
-  if __name__=='__main__':
-    main()
+if __name__ == "__main__":
+  main()
